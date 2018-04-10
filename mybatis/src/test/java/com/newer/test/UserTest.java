9d863458a6862585a8863b6b2dccf804bcaa6fbe
yanshuibin1208 <yanshuibin2013@163.com>
2018-04-10 20:34:27 +0800

    用mybatis对表author的增删改查

--- mybatis/src/test/java/com/newer/test/UserTest.java
+++ mybatis/src/test/java/com/newer/test/UserTest.java
@@ -6,6 +6,7 @@ import com.newer.util.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.sql.Date;
 import java.util.List;
 
 public class UserTest {
@@ -38,7 +39,20 @@ public class UserTest {
         SqlSession sqlSession= SqlSessionUtil.getSqlSession();
         UserDao dao=sqlSession.getMapper(UserDao.class);
         List<User> users=dao.findAll();
-        System.out.println(users.get(1).getSname());
+        System.out.println(users.get(1).getSbirthday());
+        SqlSessionUtil.closeSqlSession(sqlSession);
+    }
+    @Test
+    public void updateUser(){
+        User user=new User();
+        user.setSid(5);
+        user.setSsex("女");
+       user.setSbirthday(Date.valueOf("1985-02-25"));
+        SqlSession sqlSession= SqlSessionUtil.getSqlSession();
+        UserDao dao=sqlSession.getMapper(UserDao.class);
+        System.out.println(dao.updateUser(user));
+        //增删改要提交一下
+        sqlSession.commit();
         SqlSessionUtil.closeSqlSession(sqlSession);
     }
 }
